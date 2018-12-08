3348707a452b5693b2d64a2033c798e89789c982
Julie Ng <me@julie.io>
2018-12-08 21:21:28 +0100

    jsdoc: transitions

--- newton/transitions/fade-down.js
+++ newton/transitions/fade-down.js
@@ -1,14 +1,37 @@
+/** @namespace Transitions */
+
+/**
+ * @module FadeDown
+ * @memberOf Transitions
+ * @inner
+ */
+
+/**
+ *
+ * @param {d3Transition} transition
+ * @param {Integer} [offset] - distance along y-axis to fade down
+ */
 function circle (transition, offset = 5) {
 	transition
 		.attr('cy', (d) => d.y + offset)
 }
 
+/**
+ *
+ * @param {d3Transition} transition
+ * @param {Integer} [offset] - distance along y-axis to fade down
+ */
 function line (transition, offset = 2) {
 	transition
 		.attr('y1', (d) => d.source.y + offset)
 		.attr('y2', (d) => d.target.y + offset)
 }
 
+/**
+ *
+ * @param {d3Transition} transition
+ * @param {Integer} [offset] - distance along y-axis to fade down
+ */
 function text (transition, offset = 5) {
 	transition
 		.attr('y', (d) => d.y + offset)
--- newton/transitions/index.js
+++ newton/transitions/index.js
@@ -1,4 +1,15 @@
+/**
+ * Reusable Transitions
+ *
+ * @module Transitions
+ */
 module.exports = {
 	FadeDown: require('./fade-down'),
+
+	/**
+	 * @function fadeOut
+	 * @param {d3Transition} transition
+	 * @param {Integer} [fadeTo=0] - target opacity
+	 */
 	fadeOut: require('./fade-out')
 }
