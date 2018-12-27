fe41d4729921975bf0bf5d0d27bab25fc0bee082
Julie Ng <me@julie.io>
2018-12-27 13:05:49 +0100

    jsdoc: graph

--- newton/graph.js
+++ newton/graph.js
@@ -15,6 +15,18 @@ const defaults = {
 
 /**
  * Network Graph
+ *
+ * @fires Graph#tick
+ * @fires Graph#update
+ *
+ * @property {Integer} margin - Layout Margin
+ * @property {Integer} width - Graph Width
+ * @property {Integer} height - Graph Height
+ * @property {cola} cola - Layout Constraint Adapter
+ * @property {d3} svg - d3.js container selection that holds our svg elements, incl. labels, links and nodes.
+ * @property {Labels} labels - Labels for network nodes to be drawn
+ * @property {Links} links - Network links to be drawn
+ * @property {Nodes} nodes - Network nodes to be drawn
  */
 class Graph extends EventEmitter {
 
@@ -50,6 +62,8 @@ class Graph extends EventEmitter {
 	}
 
 	/**
+	 * Binds graph to network `update` and passes along cola's
+	 * `tick` event.
 	 *
 	 * @param {Object|GraphData} data - network graph data
 	 */
@@ -60,7 +74,24 @@ class Graph extends EventEmitter {
 			.jaccardLinkLengths(100,0.8)
 			.start(30)
 
+		/**
+		 * Tick event used to constrain layout using webcola.
+		 * Graph elements listen to this event to reposition themselves.
+		 *
+		 * @event Graph#tick
+		 */
 		this.cola.on('tick', () => this.emit('tick'))
+
+		/**
+		 * Update event which passes on network data so graph elements,
+		 * nodes and links can update themselves based on latest
+		 * real-time data.
+		 *
+		 * @event Graph#update
+		 * @type {Object}
+		 * @property {Array} nodes
+		 * @property {Array} links
+		 */
 		network.on('update', (data) => this.emit('update', data))
 	}
 
