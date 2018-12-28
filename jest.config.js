64bae8bf8cb4cac8081964d87510bdf070379173
Julie Ng <me@julie.io>
2018-12-28 11:34:43 +0100

    chore: lint

--- jest.config.js
+++ jest.config.js
@@ -68,10 +68,10 @@ module.exports = {
 
 	// An array of file extensions your modules use
 	moduleFileExtensions: [
-	  "js"
-	  // "json",
-	  // "jsx",
-	  // "node"
+		"js"
+		// "json",
+		// "jsx",
+		// "node"
 	],
 
 	// A map from regular expressions to module names that allow to stub out resources with a single module
