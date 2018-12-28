9d39f63a894a0d8931a0349cab71f20c7b7ac6c1
Julie Ng <me@julie.io>
2018-12-28 11:27:31 +0100

    chore(jest): do not test json files

--- jest.config.js
+++ jest.config.js
@@ -67,12 +67,12 @@ module.exports = {
 	// ],
 
 	// An array of file extensions your modules use
-	// moduleFileExtensions: [
-	//   "js",
-	//   "json",
-	//   "jsx",
-	//   "node"
-	// ],
+	moduleFileExtensions: [
+	  "js"
+	  // "json",
+	  // "jsx",
+	  // "node"
+	],
 
 	// A map from regular expressions to module names that allow to stub out resources with a single module
 	// moduleNameMapper: {},
