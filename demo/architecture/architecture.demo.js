a00fd4df98260419187f42465cb98be16d08828c
Julie Ng <me@julie.io>
2019-03-17 17:47:36 +0100

    chore: update demos

--- demo/architecture/architecture.demo.js
+++ demo/architecture/architecture.demo.js
@@ -1,14 +1,16 @@
 const Graph = require('./../../newton').Graph
+const ColaGraph = require('./../../newton').ColaGraph
 const Network = require('./../../newton').Network
 const data = require('./architecture.data')
 
-const graph = new Graph({
+const graph = new ColaGraph({
 	width: window.innerWidth,
 	height: window.innerHeight - 250,
 	flow: 'horizontal'
 })
 const network = new Network(data.nodes, data.linksMap, { uid: 'id' })
-graph.bind(network)
+
+graph.init().bind(network)
 
 document.querySelector('#js-delete')
 	.addEventListener('click', () => {
