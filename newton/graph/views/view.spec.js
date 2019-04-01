2e3bbe89bc1b11973f407281ae3548de11b5ebc4
Julie Ng <me@julie.io>
2019-04-01 18:03:12 +0800

    test(view): add initial coverage

---
+++ newton/graph/views/view.spec.js
@@ -0,0 +1,31 @@
+const View = require('./view')
+
+describe ('View', () => {
+	let view
+
+	beforeEach(() => {
+		view = new View()
+	})
+
+	it ('has a constructor', () => {
+		expect(new View()).not.toEqual(undefined)
+	})
+
+	it ('has a bindGraph method', () => {
+		expect(typeof view.bindGraph).toEqual('function')
+	})
+
+	describe ('placeholders for inherited classes', () => {
+		it ('has a render method', () => {
+			expect(() => {
+				view.render()
+			}).toThrow()
+		})
+
+		it ('has a position method', () => {
+			expect(() => {
+				view.position()
+			}).toThrow()
+		})
+	})
+})
