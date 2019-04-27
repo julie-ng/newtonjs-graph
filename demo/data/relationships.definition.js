b5b882f29dfde8b27109705f004515a6f36e0c6d
Julie Ng <me@julie.io>
2019-04-27 12:18:32 +0200

    data: add example to identify relationships

---
+++ demo/data/relationships.definition.js
@@ -0,0 +1,17 @@
+module.exports = {
+	nodes: [
+		{ id: 'a', status: 'up', label: 'Deep Source'},
+		{ id: 'b', status: 'up', label: 'Source'},
+		{ id: 'c', status: 'up', label: 'Highlighted Node'},
+		{ id: 'd', status: 'up', label: 'Target'},
+		{ id: 'e', status: 'up', label: 'Unrelated'},
+		{ id: 'f', status: 'up', label: 'Unrelated'},
+	],
+	links: [
+		{ source: 'a', target: 'b' },
+		{ source: 'b', target: 'c' },
+		{ source: 'c', target: 'd' },
+		{ source: 'd', target: 'e' },
+		{ source: 'b', target: 'f' },
+	]
+}
