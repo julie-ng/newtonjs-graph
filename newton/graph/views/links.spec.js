e24f4eebead737b7a8882f34ae71d6d50fab9126
Julie Ng <me@julie.io>
2019-05-12 13:28:16 +0200

    test: links

--- newton/graph/views/links.spec.js
+++ newton/graph/views/links.spec.js
@@ -20,6 +20,8 @@ describe ('Links', () => {
 			container: 'svg'
 		})
 
+		links.links = d3.select('svg').selectAll('line')
+
 		// prevent memory link in tests
 		links.setMaxListeners(1)
 	})
@@ -89,7 +91,7 @@ describe ('Links', () => {
 		let spy
 
 		beforeEach (() => {
-			links.links = d3.select('svg').selectAll('circle')
+			links.links = d3.select('svg').selectAll('line')
 			spy = jest.spyOn(links.links, 'attr')
 		})
 
@@ -100,16 +102,294 @@ describe ('Links', () => {
 		it ('calls `attr()` on d3 links', () => {
 			links.position()
 			const calls = spy.mock.calls
-			const firstParam = calls[0][0]
-			const secondParam = calls[1][0]
-			const thirdParam = calls[2][0]
-			const fourthParam = calls[3][0]
+			const x1 = calls[0]
+			const x2 = calls[1]
+			const y1 = calls[2]
+			const y2 = calls[3]
 
 			expect(spy).toHaveBeenCalledTimes(4)
-			expect(firstParam).toEqual('x1')
-			expect(secondParam).toEqual('x2')
-			expect(thirdParam).toEqual('y1')
-			expect(fourthParam).toEqual('y2')
+
+			expect(x1[0]).toEqual('x1')
+			expect(x2[0]).toEqual('x2')
+			expect(y1[0]).toEqual('y1')
+			expect(y2[0]).toEqual('y2')
+
+			expect(x1[1].toString().includes('d.source.x')).toBe(true)
+			expect(x2[1].toString().includes('d.target.x')).toBe(true)
+			expect(y1[1].toString().includes('d.source.y')).toBe(true)
+			expect(y2[1].toString().includes('d.target.y')).toBe(true)
+		})
+	})
+
+	describe ('are styled via attributes on <line> element', () => {
+		let attrSpy
+
+		beforeEach (() => {
+			attrSpy = jest.spyOn(links.links, 'attr')
+		})
+
+		afterEach (() => {
+			attrSpy.mockRestore()
+		})
+
+		describe ('setRelationships()', () => {
+			it ('sets `data-rel` attribute on link', () => {
+				links.setRelationships()
+				const attr = attrSpy.mock.calls[0][0]
+				expect(attrSpy).toHaveBeenCalledTimes(1)
+				expect(attr).toEqual('data-rel')
+			})
+
+			it ('uses _getRelationship() helper', () => {
+				links.setRelationships()
+				let cb = attrSpy.mock.calls[0][1]
+				expect(cb.toString().includes('_getRelationship(i, n)')).toBe(true)
+			})
+		})
+
+		describe ('hideUnrelated()', () => {
+			let l = linksArray[0]
+
+			it ('uses _setHidden() helper', () => {
+				links.hideUnrelated()
+				let cb = attrSpy.mock.calls[0][1]
+				expect(cb.toString().includes('_setHidden(i, n)')).toBe(true)
+			})
+
+			it ('sets `data-hidden` attribute on element', () => {
+				links.hideUnrelated()
+				expect(attrSpy).toHaveBeenCalled()
+			})
+
+			describe ('_setHidden() sets `data-hidden` attr', () => {
+				let relSpy
+
+				beforeEach (() => {
+					relSpy = jest.spyOn(links, `_getRelationship`)
+				})
+
+				afterEach (() => {
+					relSpy.mockRestore()
+				})
+
+				it (`sets to '1' if no relationship`, () => {
+					relSpy.mockReturnValue('has-no-relationship')
+					expect(links._setHidden(l, 'orphan')).toEqual('1')
+				})
+
+				it (`sets to '' if has relationship`, () => {
+					relSpy.mockReturnValue('is-target')
+					expect(links._setHidden(l, l.target)).toEqual('')
+				})
+			})
+		})
+
+		describe ('showAll()', () => {
+			it ('resets `data-hidden` attribute', () => {
+				links.showAll()
+				let fn = attrSpy.mock.calls[0]
+				expect(fn[0]).toEqual('data-hidden')
+				expect(fn[1]).toBe(null)
+			})
+		})
+
+		describe ('svg arrowheads', () => {
+			const nodeStub = linksArray[0]
+			let attrSpy
+
+			beforeEach (() => {
+				attrSpy = jest.spyOn(links.links, 'attr')
+			})
+
+			afterEach (() => {
+				attrSpy.mockRestore()
+			})
+
+			describe ('setRelationships()', () => {
+				xit ('hides nodes without relations', () => {
+					let spy = jest.spyOn(links, 'hideUnrelated')
+					links.setRelationships()
+					expect(spy).toHaveBeenCalledTimes(1)
+				})
+
+				xdescribe ('option: markers', () => {
+					let spy
+
+					beforeEach(() => {
+						spy = jest.spyOn(links, 'showArrows')
+					})
+
+					afterEach (() => {
+						spy.mockRestore()
+					})
+
+					it ('defaults to true', () => {
+						let emptyOpts = {}
+						links.setRelationships(nodeStub, emptyOpts)
+						expect(spy.mock.calls[0][1].markers).toBe(true)
+					})
+
+					it ('delegates to showArrows() when true', () => {
+						links.setRelationships(nodeStub, { markers: true })
+						expect(spy).toHaveBeenCalledTimes(1)
+					})
+
+					it ('ignores showArrows() when false', () => {
+						links.setRelationships(nodeStub, { markers: false })
+						expect(spy).not.toHaveBeenCalled()
+					})
+				})
+			})
+
+			describe ('_getMarkerEnd(i, node, opts)', () => {
+				const colorStyles = [
+					'url(#is-source)',
+					'url(#is-deep-source)',
+					'url(#is-target)',
+					'url(#has-no-relationship)'
+				]
+
+				const deepSourceMock = jest.fn()
+				const graphStub = {
+					isDeepSourceLink: deepSourceMock,
+					on: () => {}
+				}
+
+				beforeEach(() => {
+					links.bindGraph(graphStub)
+				})
+
+				afterEach(() => {
+					deepSourceMock.mockRestore()
+				})
+
+				it ('gets relationship to node', () => {
+					let spy = jest.spyOn(links, '_getRelationship')
+					let l = linksArray[0]
+					links._getMarkerEnd(l, l.target)
+					expect(spy).toHaveBeenCalledTimes(1)
+				})
+
+				describe ('option: `color`', () => {
+					describe ('= true', () => {
+						it ('colors based on relationships', () => {
+							let l = linksArray[0]
+							let style = links._getMarkerEnd(l, l.target, { color: true })
+							expect(colorStyles.includes(style)).toBe(true)
+						})
+					})
+
+					describe ('= false', () => {
+						it ('uses `is-default`', () => {
+							let l = linksArray[0]
+							let style = links._getMarkerEnd(l, l.target, { color: false, showAll: true })
+							expect(style).toEqual('url(#is-default)')
+						})
+					})
+				})
+
+				describe ('when: links have NO relationship', () => {
+					let l = linksArray[0]
+
+					describe ('option: `showAll`', () => {
+						it ('defaults to true', () => {
+							let style = links._getMarkerEnd(l, 'orphan-node', {})
+							expect(style).toEqual('url(#is-default)')
+						})
+
+						describe ('= true', () => {
+							it ('returns a default marker', () => {
+								let style = links._getMarkerEnd(l, 'orphan-node', { showAll: true })
+								expect(style).toEqual('url(#is-default)')
+							})
+						})
+
+						describe ('= false', () => {
+							it ('returns an empty string', () => {
+								let style = links._getMarkerEnd(l, 'orphan-node', { showAll: false })
+								expect(style).toEqual('')
+							})
+						})
+					})
+				})
+			})
+
+			describe ('resetStyles()', () => {
+				it ('adds removes arrows', () => {
+					links.resetStyles()
+					const calls = attrSpy.mock.calls
+					expect(attrSpy).toHaveBeenCalledTimes(3)
+					expect(calls[0][0]).toEqual('data-rel')
+					expect(calls[1][0]).toEqual('marker-end')
+					expect(calls[2][0]).toEqual('data-hidden')
+				})
+			})
+		})
+	})
+
+
+	describe ('helpers', () => {
+
+		describe ('_getRelationship()', () => {
+			const deepSourceMock = jest.fn()
+			const graphStub = {
+				isDeepSourceLink: deepSourceMock,
+				on: () => {}
+			}
+
+			beforeEach(() => {
+				links.bindGraph(graphStub)
+			})
+
+			afterEach(() => {
+				deepSourceMock.mockRestore()
+			})
+
+			const link = { source: 'a', target: 'b' }
+
+			it ('can identify sources', () => {
+				expect(links._getRelationship(link, 'a')).toEqual('is-source')
+			})
+
+			it ('can identify targets', () => {
+				expect(links._getRelationship(link, 'b')).toEqual('is-target')
+			})
+
+			// avoid mock setup in other places
+			describe ('is used by:', () => {
+				let spy
+				let l = linksArray[0]
+
+				beforeEach (() => {
+					spy = jest.spyOn(links, '_getRelationship')
+				})
+
+				afterEach(() => {
+					spy.mockRestore()
+				})
+
+				it ('_setHidden()', () => {
+					links._setHidden(l, 'n')
+					expect(spy).toHaveBeenCalled()
+				})
+
+				it ('_getMarkerEnd()', () => {
+					links._getMarkerEnd(l, 'n')
+					expect(spy).toHaveBeenCalled()
+				})
+			})
+
+			describe ('not direct source or target', () => {
+				it ('delegates no matches to graph.isDeepSourceLink()', () => {
+					deepSourceMock.mockReturnValue(true)
+					expect(links._getRelationship(link, 'not-a-node')).toEqual('is-deep-source')
+				})
+
+				it ('can `has-no-relationship`', () => {
+					deepSourceMock.mockReturnValue(false)
+					expect(links._getRelationship(link, 'not-a-node')).toEqual('has-no-relationship')
+				})
+			})
 		})
 	})
 })
